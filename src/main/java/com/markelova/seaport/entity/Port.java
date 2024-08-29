35eedd342a9bbcb2058de6bd889dcdaa266acca3
Iuliia <markelova.uv@gmail.com>
2024-08-29 15:35:32 +0300

    2024-08-29 15:35 create Dock class and add field to Port class and add test

--- src/main/java/com/markelova/seaport/entity/Port.java
+++ src/main/java/com/markelova/seaport/entity/Port.java
@@ -1,13 +1,22 @@
 package com.markelova.seaport.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Port {
     private String name;
+    private List<Dock> docks = new ArrayList<>();
 
     public Port() {
     }
 
     public Port(String name) {
         this.name = name;
+    }
+
+    public Port(String name, List<Dock> docks) {
+        this.name = name;
+        this.docks = docks;
     }
 
     public String getName() {
@@ -18,8 +27,20 @@ public class Port {
         this.name = name;
     }
 
+    public List<Dock> getDocks() {
+        return this.docks;
+    }
+
+    public void setDocks(List<Dock> docks) {
+        this.docks = docks;
+    }
+
+    public void addDock(Dock dock) {
+        this.docks.add(dock);
+    }
+
     @Override
     public String toString() {
-        return String.format("Port | name: %s", this.name);
+        return String.format("Port | name: %s | docks: %s", this.name, this.docks);
     }
 }
