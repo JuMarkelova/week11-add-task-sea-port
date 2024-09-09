03f452e7301b8a939ce4c6f6530654f5ad21d64d
Iuliia <markelova.uv@gmail.com>
2024-09-09 12:13:30 +0300

    feature: 2024-09-09 12:13 Create Dispatcher class

---
+++ src/main/java/com/markelova/seaport/entity/Dispatcher.java
@@ -0,0 +1,11 @@
+package com.markelova.seaport.entity;
+
+public class Dispatcher {
+
+    Dispatcher() {
+    }
+
+    public void print(Object object) {
+        System.out.println(object);
+    }
+}
